497db87de22444984503ecc4b2feec62cc8e2b94
salmanx150 <salmankhanlashari3635@gmail.com>
2024-08-25 12:44:11 +0000

    yes

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+
+const accountId =234567
+let accountEmail = "salman@.com"
+let accountpassword = "5666"
+let city = "lahore"
+city = "jaipur"
+console.log( accountId)
+console.table([accountEmail, accountpassword, city])
+
+
