f48373d7fc9e4091ca631962f64744ca83ae23bd
salmanx150 <salmankhanlashari3635@gmail.com>
2024-08-25 12:44:48 +0000

    yes

---
+++ 01_basics/data.js
@@ -0,0 +1,12 @@
+"use strict"; //apply new version of js
+// alert(3+
+// 3) //use in browser this is node.js
+// code read abiity is must be high
+
+// there or is many datatypes but in java script easy to use data types not need to definned data types in javascript this easy to use
+let name = "hetesh" //this string
+let age = 21 //this number
+let isloggedin = true  //this is false
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "");
